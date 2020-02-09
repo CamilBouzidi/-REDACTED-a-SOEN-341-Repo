1b044485b17dd6ecc89203d07261f06e17f280cf
laurentlaurent <lao.laurent@gmail.com>
2020-02-09 01:53:10 -0500

    [#48] Refactor .CSS into .SCSS

--- src/app/page-landing/page-landing.component.ts
+++ src/app/page-landing/page-landing.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-landing',
   templateUrl: './page-landing.component.html',
-  styleUrls: ['./page-landing.component.css']
+  styleUrls: ['./page-landing.component.scss']
 })
 export class PageLandingComponent implements OnInit {
 
