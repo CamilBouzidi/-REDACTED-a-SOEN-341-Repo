e303c1a3099653f972cfaaac705e04389efe1171
Tommy Josépovic <tommy.josepovic@gmail.com>
2020-02-13 23:21:59 -0500

    [#83] Remove zombie code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,23 +6,7 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  private postsCollection: AngularFirestoreCollection<any>;
-
   title = 'ShutterFeed';
-  posts: Observable<any[]>;
 
-  constructor(afs: AngularFirestore) {
-    this.postsCollection = afs.collection<any>('posts');
-    this.posts = this.postsCollection.snapshotChanges().pipe(
-      map(actions => actions.map(a => {
-        const data = a.payload.doc.data();
-        const id = a.payload.doc.id;
-        return { id, ...data };
-      }))
-    );
-  }
-
-  output(post) {
-    console.log(post);
-  }
+  constructor() {}
 }
