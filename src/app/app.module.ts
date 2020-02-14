a019ea454f3e4324ad961bfb475837abee90e5ec
laurentlaurent <lao.laurent@gmail.com>
2020-02-13 23:04:27 -0500

    [#71] Add login/logout button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,7 +43,7 @@ import { LoginButtonComponent } from './login-button/login-button.component';
     NewPostDialogComponent,
     FeedComponent,
     FeedCardComponent,
-    LoginButtonComponent
+    LoginButtonComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/login-button/login-button.component.ts
+++ src/app/login-button/login-button.component.ts
@@ -1,15 +1,27 @@
+// Adding back Alexander's work
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { AuthService } from '../core/auth.service';
 
 @Component({
   selector: 'app-login-button',
   templateUrl: './login-button.component.html',
-  styleUrls: ['./login-button.component.css']
+  styleUrls: ['./login-button.component.scss']
 })
 export class LoginButtonComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    const loginButton = document.getElementById('login_button');
+    loginButton.style.display = 'block';
   }
 
+  onLoginClick() {
+    this.router.navigateByUrl('/user-profile');
+  }
+
+  onLogoutClick() {
+    this.auth.signOut();
+  }
 }
