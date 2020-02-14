f328561eb2446b075e5d6683c3d9abe8531d4b11
tommy-josepovic <tommy-josepovic@users.noreply.github.com>
2020-02-14 15:34:45 -0500

    Sprint 1 fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 /* Other imports */
 import { AppComponent } from './app.component';
@@ -70,7 +71,8 @@ import { LoginButtonComponent } from './login-button/login-button.component';
     MatInputModule,
     MatFormFieldModule,
     MatSnackBarModule,
-    MatCardModule
+    MatCardModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login-button/login-button.component.ts
+++ src/app/login-button/login-button.component.ts
@@ -1,5 +1,5 @@
 // Adding back Alexander's work
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {Router} from '@angular/router';
 import { AuthService } from '../core/auth.service';
 
@@ -8,14 +8,9 @@ import { AuthService } from '../core/auth.service';
   templateUrl: './login-button.component.html',
   styleUrls: ['./login-button.component.scss']
 })
-export class LoginButtonComponent implements OnInit {
+export class LoginButtonComponent {
 
   constructor(public auth: AuthService, private router: Router) { }
-
-  ngOnInit(): void {
-    const loginButton = document.getElementById('login_button');
-    loginButton.style.display = 'block';
-  }
 
   onLoginClick() {
     this.router.navigateByUrl('/user-profile');
