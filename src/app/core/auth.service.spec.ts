b924170b3a75c0299314b753d611b4fb1ffb925f
laurentlaurent <lao.laurent@gmail.com>
2020-02-22 22:59:12 -0500

    Implement Auth.service.spec.ts to verify AngularFireAuth usage

--- src/app/core/auth.service.spec.ts
+++ src/app/core/auth.service.spec.ts
@@ -1,16 +1,53 @@
+
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {AngularFireStorage, AngularFireStorageModule} from '@angular/fire/storage';
+import {Router} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AngularFireModule} from '@angular/fire';
+import {kebabToCamelCase} from 'codelyzer/util/utils';
+import {firebaseConfig} from '../../environments/secrets/firebaseConfig';
+import {AngularFireDatabase, AngularFireDatabaseModule} from '@angular/fire/database';
+
+/* The class looks like this:
+export class AuthService {
+
+  user$: Observable<any>;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  )
+ */
 
 describe('AuthService', () => {
-  /*let service: AuthService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+  let afAuth: AngularFireAuth;
+  let afStorage: AngularFireStorage;
+  let router: RouterTestingModule;
+
+  beforeEach(async () => {
+
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireDatabaseModule,
+        AngularFireStorageModule
+      ],
+      providers: [
+        AngularFireAuth,
+        AngularFireStorage,
+        Router
+      ]
+    }).compileComponents();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  }); */
+  it('should use AngularFireAuth', () => {
+    afAuth = TestBed.inject(AngularFireAuth);
+    expect(afAuth).toBeTruthy();
+  });
+
 });
